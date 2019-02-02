f296b923da078338f31b16922e806d9a845906d7
周文波 <zhouwenbo03@meituan.com>
2019-02-02 15:27:16 +0800

    Initial Commit

---
+++ pages/logs/logs.js
@@ -0,0 +1,28 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+  }
+})
+
+Component({
+  pageLifetimes: {
+    show() {
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 1
+        })
+      }
+    }
+  }
+})
