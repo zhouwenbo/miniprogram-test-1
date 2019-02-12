02a2c31e2472d8bd05451a65a3e84598fe7b19d7
周文波 <zhouwenbo03@meituan.com>
2019-02-12 15:54:05 +0800

    [feat]增加了首页底tab的功能。遇到一些坑
    [feat]增加了首页底tab的功能。遇到一些坑

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -4,12 +4,12 @@ Component({
     color: "#7A7E83",
     selectedColor: "#3cc51f",
     list: [{
-      pagePath: "pages/index/index",
+      pagePath: "/pages/index/index",
       iconPath: "/image/icon_component.png",
       selectedIconPath: "/image/icon_component_HL.png",
       text: "组件"
     }, {
-      pagePath: "pages/logs/logs",
+      pagePath: "/pages/logs/logs",
       iconPath: "/image/icon_API.png",
       selectedIconPath: "/image/icon_API_HL.png",
       text: "接口"
@@ -21,7 +21,8 @@ Component({
     switchTab(e) {
       const data = e.currentTarget.dataset
       const url = data.path
-      wx.switchTab({url})
+      console.log('url = ' + url)
+      wx.switchTab({ url })
       this.setData({
         selected: data.index
       })
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,20 +1,11 @@
-//logs.js
+// pages/logs/logs.js
+
 const util = require('../../utils/util.js')
 
-Page({
+Component({
   data: {
     logs: []
   },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
-})
-
-Component({
   pageLifetimes: {
     show() {
       if (typeof this.getTabBar === 'function' &&
@@ -23,6 +14,24 @@ Component({
           selected: 1
         })
       }
+    }
+  },
+  lifetimes: {
+    // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+    attached() {
+      console.log("attached")
+      this.setData({
+        logs: (wx.getStorageSync('logs') || []).map(log => {
+          var d = util.formatTime(new Date(log))
+          return d
+        })
+      })
+    },
+    moved() {
+      console.log("moved")
+    },
+    detached() {
+      console.log("detached")
     }
   }
 })
